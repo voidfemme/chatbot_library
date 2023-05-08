30dd0b562c130e039d13e63a5acd44c6cfef1520
voidfemme <rose.proctor@pcc.edu>
2023-05-07 19:16:40 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chatbot-library",
+    version="0.1.0",
+    description="A Python library for creating chatbots using GPT-based models",
+    author="voidfemme",
+    url="https://github.com/yourusername/chatbot-library",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "tiktoken",
+        "google-api-python-client",
+        "beautifulsoup4",
+        "colored",
+        "requests",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPL v3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
