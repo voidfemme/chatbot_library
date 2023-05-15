b1254d6452dfcd3a1af865cb30a814ac81d32d60
voidfemme <rose.proctor@pcc.edu>
2023-05-15 04:51:34 -0700

    changed version number to reflect recent changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chatbot-library",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python library for creating chatbots using GPT-based models",
     author="voidfemme",
     url="https://github.com/yourusername/chatbot-library",
